4538202959b974348f7cf40eb9c1911f1c26d5f1
ask-r0 <75451424+ask-r0@users.noreply.github.com>
2021-06-25 20:34:16 +0200

    Controller for tic tac to game (first version)

---
+++ src/main/java/no/game/tictactoe/controller/GameViewController.java
@@ -0,0 +1,40 @@
+package no.game.tictactoe.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import no.game.tictactoe.model.Board;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GameViewController implements Initializable {
+
+    @FXML private GridPane grid;
+    private Board gameBoard;
+
+    @FXML
+    void onSquareClicked(MouseEvent event) {
+        Node source = event.getPickResult().getIntersectedNode();
+        Integer colIndex = (GridPane.getColumnIndex(source) == null) ?  0 : (GridPane.getColumnIndex(source));
+        Integer rowIndex = (GridPane.getRowIndex(source) == null) ? 0 : (GridPane.getRowIndex(source));
+        int sq = 0;
+        if(gameBoard.isSqEmpty(sq)) {
+            gameBoard.makeMove(move); //update turn
+        } else {
+
+        }
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        gameBoard = new Board();
+
+
+    }
+
+
+}
