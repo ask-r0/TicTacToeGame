53d053918b5e268922cc2cce9f960c2a98d26b95
ask-r0 <75451424+ask-r0@users.noreply.github.com>
2021-06-25 20:35:13 +0200

    Loaded game view fxml, working

--- src/main/java/no/game/tictactoe/main/App.java
+++ src/main/java/no/game/tictactoe/main/App.java
@@ -1,19 +1,31 @@
 package no.game.tictactoe.main;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import no.game.tictactoe.model.Board;
 
-import java.io.Console;
+import java.io.File;
 import java.io.IOException;
-import java.util.zip.CheckedOutputStream;
+import java.net.URL;
 
 public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+
+        URL url = new File("src/main/resources/no.game.tictactoe.controller/GameView.fxml").toURI().toURL();
+        FXMLLoader fxmlLoader = new FXMLLoader(url);
+        Parent root = fxmlLoader.load();
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
     }
 
-    public static void main(String[] args) { launch(); }
+    public static void main(String[] args) {
+        launch();
+    }
 }
